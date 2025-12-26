83d6322bcf07001ef312821342da099461882e48
philippe44 <philippe44@users.noreply.github.com>
2025-12-26 21:14:07 +0100

    Update client_info.h

--- components/spotify/client_info.h
+++ components/spotify/client_info.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define CLIENT_ID "<your spotify client's id>"
-#define CLIENT_SECRET "<your spotify client's secret>"
+//#define CLIENT_ID "<your spotify client's id>"
+//#define CLIENT_SECRET "<your spotify client's secret>"
